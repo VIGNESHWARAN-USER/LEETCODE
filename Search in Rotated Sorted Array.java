b9bbb58e60796c0347e5c61728d21e87504e22be
VIGNESHWARAN-USER <147700262+VIGNESHWARAN-USER@users.noreply.github.com>
2024-08-06 05:58:08 +0530

    Create Search in Rotated Sorted Array.java

---
+++ Search in Rotated Sorted Array.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+
+        int left = 0, right = nums.length - 1;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] > nums[right]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+
+        int pivot = left;
+        left = 0;
+        right = nums.length - 1;
+
+        if (target >= nums[pivot] && target <= nums[right]) {
+            left = pivot;
+        } else {
+            right = pivot - 1;
+        }
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+}
